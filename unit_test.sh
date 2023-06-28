e4054ff13c1827e407f1f01d715bdebf4f255fbc
Justin Orringer <justinorringer@gmail.com>
2023-06-28 17:34:55 -0400

    Working with docker locally

--- unit_test.sh
+++ unit_test.sh
@@ -2,7 +2,8 @@
 set -x
 
 TEST_RESULT=0
-GO_TOOLSET_IMAGE='registry.access.redhat.com/ubi9/go-toolset:1.18.9'
+DOCKERFILE='Dockerfile-test'
+IMAGE='changelog'
 TEARDOWN_RAN=0
 
 teardown() {
@@ -15,7 +16,7 @@ teardown() {
     TEARDOWN_RAN=1
 }
 
-trap teardown EXIT ERR SIGINT SIGTERM
+# trap teardown EXIT ERR SIGINT SIGTERM
 
 mkdir -p artifacts
 
@@ -28,17 +29,13 @@ get_N_chars_commit_hash() {
 
 TEST_CONTAINER_NAME="changelog-$(get_N_chars_commit_hash 7)"
 
-docker run -d --name "$TEST_CONTAINER_NAME" \
-    "$GO_TOOLSET_IMAGE" sleep infinity
+docker build -f "$DOCKERFILE" -t "$IMAGE" .
 
-docker cp -a . "$TEST_CONTAINER_NAME:/workdir"
+docker run -d --rm --name "$TEST_CONTAINER_NAME" "$IMAGE" sleep infinity
 
 docker exec --workdir /workdir "$TEST_CONTAINER_NAME" make install > 'artifacts/install_logs.txt'
 docker exec --workdir /workdir -e PATH=/opt/app-root/src/go/bin:$PATH "$TEST_CONTAINER_NAME" make test > 'artifacts/test_logs.txt'
 TEST_RESULT=$?
-
-docker cp "$TEST_CONTAINER_NAME:/workdir/cover.out" 'artifacts/cover.out'
-docker cp "$TEST_CONTAINER_NAME:/workdir/junit-changelog.xml" 'artifacts/junit-changelog.xml'
 
 if [ $TEST_RESULT -eq 0 ]; then
     echo "tests ran successfully"
