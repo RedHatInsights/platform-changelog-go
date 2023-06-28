735cded62b88f5c9a424218e6660cd6f7b6fc188
Justin Orringer <justinorringer@gmail.com>
2023-06-28 17:48:41 -0400

    Prettied up the output

--- unit_test.sh
+++ unit_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 
 TEST_RESULT=0
 DOCKERFILE='Dockerfile-test'
@@ -16,7 +15,7 @@ teardown() {
     TEARDOWN_RAN=1
 }
 
-# trap teardown EXIT ERR SIGINT SIGTERM
+trap teardown EXIT ERR SIGINT SIGTERM
 
 mkdir -p artifacts
 
@@ -29,17 +28,31 @@ get_N_chars_commit_hash() {
 
 TEST_CONTAINER_NAME="changelog-$(get_N_chars_commit_hash 7)"
 
+echo "Building image"
 docker build -f "$DOCKERFILE" -t "$IMAGE" .
 
+echo -e "\n---------------------------------------------------------------\n"
+
+echo "Running container"
 docker run -d --rm --name "$TEST_CONTAINER_NAME" "$IMAGE" sleep infinity
 
+echo -e "\n---------------------------------------------------------------\n"
+
+echo "Installing dependencies"
 docker exec --workdir /workdir "$TEST_CONTAINER_NAME" make install > 'artifacts/install_logs.txt'
+
+echo -e "\n---------------------------------------------------------------\n"
+echo "Running tests"
 docker exec --workdir /workdir -e PATH=/opt/app-root/src/go/bin:$PATH "$TEST_CONTAINER_NAME" make test > 'artifacts/test_logs.txt'
 TEST_RESULT=$?
 
+cat artifacts/test_logs.txt
+
+echo -e "\n---------------------------------------------------------------\n"
+
 if [ $TEST_RESULT -eq 0 ]; then
-    echo "tests ran successfully"
+    echo "Tests ran successfully"
 else
-    echo "tests failed"
+    echo "Tests failed..."
     sh "exit 1"
 fi
