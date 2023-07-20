736d865bdb5987f84031dffe4a74c1f1b782c3e6
Justin Orringer <justinorringer@gmail.com>
2023-07-20 15:42:29 -0400

    More logging around content-type

--- internal/endpoints/github.go
+++ internal/endpoints/github.go
@@ -36,7 +36,7 @@ type GithubCommit struct {
 
 func decodeGithubJSONBody(w http.ResponseWriter, r *http.Request) (GithubPayload, error) {
 	if !strings.Contains(r.Header.Get("Content-Type"), "application/json") {
-		return nil, fmt.Errorf("invalid Content-Type header")
+		return nil, fmt.Errorf("invalid Content-Type header: '%s' should be 'application/json'", r.Header.Get("Content-Type"))
 	}
 
 	if r.Body == nil {
